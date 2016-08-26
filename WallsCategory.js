bc17b86ad8893fb6af4b5f8508e72a934c105e75
Hiten Soni <soni_hiten@yahoo.com>
2016-08-26 15:44:51 +0530

    12
    12

--- WallsCategory.js
+++ WallsCategory.js
@@ -7,7 +7,7 @@
              "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Soul.png"
           },
           {
-             "category_name": "Stripes & Pattern",
+             "category_name": "Stripes & Patterns",
              "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/RiseUi.png"
           },
           {
