8241dc719017e21d6a52dfaa7922d47a845d445d
Hiten Soni <soni_hiten@yahoo.com>
2016-08-26 15:18:03 +0530

    ct
    ct1

---
+++ WallsCategory.js
@@ -0,0 +1,26 @@
+{
+	"Categories":
+	[
+
+          {
+             "category_name": "Material Style",
+             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Soul.png"
+          },
+          {
+             "category_name": "Material Style",
+             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Vibes.png"
+          },
+          {
+             "category_name": "Material Style",
+             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Fusion.png"
+          },
+          {
+             "category_name": "Stripes & Patterns",
+             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/RiseUi.png"
+          },
+          {
+             "category_name": "Stripes & Patterns",
+             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Ace.png"
+          }
+	]
+}
