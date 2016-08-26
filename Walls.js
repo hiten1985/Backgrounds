4040b18a4f8ef1f776759d8b61be73bb333ae979
Hiten Soni <soni_hiten@yahoo.com>
2016-08-26 15:42:48 +0530

    1

--- Walls.js
+++ Walls.js
@@ -192,6 +192,65 @@
        }
                  
        ],
+"Material":
+         [
+       {
+		"author": "Hiten Soni",
+	        "name": "Soul",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Soul.png"
+               
+     },
+     {
+		"name": "Vibes",
+                "author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Vibes.png"
+     },
+     {
+		"name": "Fusion",
+                "author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Fusion.png"
+     },
+     {
+		"name": "Inspire",
+                "author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Inspire.png"
+     },
+     {
+		"name": "Material 1",
+                "author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Material1.png"
+	},
+     {
+		"name": "CardUi",
+                "author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/CardUi.png"
+  },
+     {
+		"name": "Stripes",
+                "author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Stripes.png"
+	 },
+         {
+		"name": "Stripes 2",
+                "author": "Hiten Soni",
+                "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Stripes2.png"
+	},
+        {
+		"name": "PerfectNoteUi",
+                "author": "Tim Austin",
+                "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/PerfectNoteUi.png"
+	},
+        {
+		"name": "PerfectUi",
+                "author": "Tim Austin",
+                "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/PerfectUi.png"
+	},
+        {
+		"name": "SupremeUi",
+                "author": "Tim Austin",
+                "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/SupremeUi.png"
+	}
+],
 "Stripes & Patterns":
          [
      {
--- WallsCategory.js
+++ WallsCategory.js
@@ -3,24 +3,20 @@
 	[
 
           {
-             "category_name": "Material Style",
+             "category_name": "Material",
              "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Soul.png"
           },
           {
-             "category_name": "Material Style",
-             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Vibes.png"
-          },
-          {
-             "category_name": "Material Style",
-             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Fusion.png"
-          },
-          {
-             "category_name": "Stripes & Patterns",
+             "category_name": "Stripes & Pattern",
              "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/RiseUi.png"
           },
           {
-             "category_name": "Stripes & Patterns",
-             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Ace.png"
+             "category_name": "Bokeh",
+             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Oxygen.png"
+          },
+          {
+             "category_name": "Misc",
+             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Mkbhd.png"
           }
 	]
 }
