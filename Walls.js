e0d452c7d7add62363a3f3d05c461ec1ec2dc0d2
Hiten Soni <soni_hiten@yahoo.com>
2016-08-26 14:34:19 +0530

    Final fix
    Final Fix

--- Walls.js
+++ Walls.js
@@ -64,7 +64,132 @@
 		"name": "SupremeUi",
                 "author": "Tim Austin",
                 "url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/SupremeUi.png"
-	}
+	},
+        {
+        "name":"Rise Ui",
+        "author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/RiseUi.png"
+     },
+      {
+        "name":"Ace",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Ace.png"
+     },
+     {
+        "name":"Arena",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Arena.png"
+     },
+    {
+        "name":"Arena1",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Arena1.png"
+     },
+     {
+        "name":"Weyo",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Weyo.png"
+     },
+     {
+        "name":"Ascend",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Ascend.png"
+      },
+     {
+        "name":"Eco",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Eco.png"
+     },
+     {
+        "name":"Prime",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Prime.png"
+     },
+     {
+        "name":"Radiant",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Radiant.png"
+     },
+     {
+        "name":"Oxygen",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Oxygen.png"
+      },
+      {
+        "name":"Bokeh 1",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh_1.png"
+       },
+      {
+        "name":"Bokeh 2",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh_2.png"
+     },
+      {
+        "name":"Bokeh 3",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh_3.png"
+       },
+      {
+        "name":"Bokeh 4",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh4.png"
+      },
+      {
+        "name":"Mkbhd",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Mkbhd.png"
+      },
+      {
+        "name":"Mountain",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Mountain.png"
+      },
+      {
+        "name":"Mountain",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Mountain_Trials.png"
+      },
+      {
+        "name":"Mountain 2",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Mountain2.png"
+      },
+      {
+        "name":"Mountain 3",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Mountain3.png"
+       },
+       {
+        "name":"Sea",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Sea.png"
+       },
+      {
+        "name":"Noise",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Noise.png"
+       },
+      {
+        "name":"Random",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/RandomShapes.png"
+       },
+      {
+        "name":"Deep",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Deep.JPG"
+      },
+      {
+        "name":"Geo",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Geo.png"
+       },
+      {
+        "name":"Ore",
+	"author": "Hiten Soni",
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Ore.png"
+       }
                  
        ],
 "Stripes & Patterns":
