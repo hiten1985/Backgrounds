56302fee610b2be1fabc30c017bf0bf38be3c0cd
Hiten Soni <soni_hiten@yahoo.com>
2016-08-27 21:32:21 +0530

    Changed site
    Changed Site for walls

--- GITHUB JS/Walls.js
+++ GITHUB JS/Walls.js
@@ -309,22 +309,22 @@
       {
         "name":"Bokeh 1",
 	"author": "Hiten Soni",
-        "url":"http://www.hitensoni.com/wp-content/uploads/2016/08/Bokeh_1.png"
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh_1.png"
        },
       {
         "name":"Bokeh 2",
 	"author": "Hiten Soni",
-        "url":"http://www.hitensoni.com/wp-content/uploads/2016/08/Bokeh_2.png"
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh_2.png"
      },
       {
         "name":"Bokeh 3",
 	"author": "Hiten Soni",
-        "url":"http://www.hitensoni.com/wp-content/uploads/2016/08/Bokeh_3.png"
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh_3.png"
        },
       {
         "name":"Bokeh 4",
 	"author": "Hiten Soni",
-        "url":"http://www.hitensoni.com/wp-content/uploads/2016/08/Bokeh4.png"
+        "url":"https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh4.png"
       }
   ],
 
