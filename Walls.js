de54603a47bad2a7095fe6ab0101458673770924
Hiten Soni <soni_hiten@yahoo.com>
2016-08-29 12:33:05 +0530

    PS6
    PS6

--- Walls.js
+++ Walls.js
@@ -182,8 +182,37 @@
 		"name": "Photography",
 		"author": "Hiten Soni",
 		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_1.jpg"
+		},
+		{
+		"name": "Photography 2",
+		"author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_2.jpg"
+		},
+	 	{
+		"name": "Photography 3,
+		"author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_3.jpg"
+		},
+		{
+		"name": "Photography 4",
+		"author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_4.jpg"
+		},
+		{
+		"name": "Photography 5",
+		"author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_5.jpg"
+		},
+		{
+		"name": "Photography 6",
+		"author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_6.jpg"
+		},
+		{
+		"name": "Photography 7",
+		"author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_7.jpg"
 		}
-
 	],
 	"Material": [{
 		"author": "Hiten Soni",
@@ -381,7 +410,37 @@
 		"name": "Photography",
 		"author": "Hiten Soni",
 		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_1.jpg"
-	}
+	},
+	{
+		"name": "Photography 2",
+		"author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_2.jpg"
+		},
+	 	{
+		"name": "Photography 3,
+		"author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_3.jpg"
+		},
+		{
+		"name": "Photography 4",
+		"author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_4.jpg"
+		},
+		{
+		"name": "Photography 5",
+		"author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_5.jpg"
+		},
+		{
+		"name": "Photography 6",
+		"author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_6.jpg"
+		},
+		{
+		"name": "Photography 7",
+		"author": "Hiten Soni",
+		"url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_7.jpg"
+		}
     
    ]
 
