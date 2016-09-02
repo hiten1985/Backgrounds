8da8804db6dbd2df4c07e93d8b8a119e32f28839
Hiten Soni <soni_hiten@yahoo.com>
2016-09-02 21:35:15 +0530

    Add files via upload

---
+++ AllJson/WallsCategoryFireBase.js
@@ -0,0 +1,30 @@
+{
+	"Categories":
+	[
+
+          {
+             "category_name": "Material",
+             "category_url": "https://firebasestorage.googleapis.com/v0/b/backgrounds-2e8eb.appspot.com/o/Backgrounds%2FMaterial%2FMaterial_8.png?alt=media&token=40acb768-c6e5-4fe6-9326-ef9163d3d1b4"
+          },
+          {
+             "category_name": "Stripes & Patterns",
+             "category_url": "https://firebasestorage.googleapis.com/v0/b/backgrounds-2e8eb.appspot.com/o/Backgrounds%2FStripesandpatterns%2FRadiant.png?alt=media&token=f948ac73-0a21-45e9-8193-581bd5ce6fd3"
+          },
+          {
+             "category_name": "Bokeh",
+             "category_url": "https://firebasestorage.googleapis.com/v0/b/backgrounds-2e8eb.appspot.com/o/Backgrounds%2FBokeh%2FBokeh_1.png?alt=media&token=7dcf9a04-c039-40ce-aa0a-44c9346fc2bdg"
+          },
+          {
+             "category_name": "Mountains",
+             "category_url": "https://firebasestorage.googleapis.com/v0/b/backgrounds-2e8eb.appspot.com/o/Backgrounds%2FMountains%2FMoon.png?alt=media&token=3f4c6f3c-5397-422e-8f41-b2fa455b9d1d"
+          },
+          {
+             "category_name": "Misc",
+             "category_url": "https://firebasestorage.googleapis.com/v0/b/backgrounds-2e8eb.appspot.com/o/Backgrounds%2FMisc%2FMkbhd.png?alt=media&token=a38b637a-b29e-48eb-85f6-648d8241c700"
+          },
+          {
+             "category_name": "Photography",
+             "category_url": "https://firebasestorage.googleapis.com/v0/b/backgrounds-2e8eb.appspot.com/o/Backgrounds%2FPhotography%2FPhotography_9.jpg?alt=media&token=3c69e110-adc3-4f07-b09b-d1de49ec3f59"
+          }
+	]
+}
