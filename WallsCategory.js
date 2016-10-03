93390abbc5da4ddc08f7f7038b692b21dd95577b
Hiten Soni <soni_hiten@yahoo.com>
2016-10-03 13:14:38 +0530

    2
    2

--- WallsCategory.js
+++ WallsCategory.js
@@ -4,35 +4,35 @@
 
           {
              "category_name": "Material",
-             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Material_8.png"
+             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Material/Material_8.png"
           },
           {
              "category_name": "Stripes & Patterns",
-             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Radiant.png"
+             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/StripesandPatterns/Radiant.png"
           },
           {
              "category_name": "Bokeh",
-             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Bokeh_3.png"
+             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Bokeh/Bokeh_3.png"
           },
           {
              "category_name": "Mountains",
-             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Moon.png"
+             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Mountains/Moon.png"
           },
           {
              "category_name": "Outer Space",
-             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Space.png"
+             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/OuterSpace/Space.png"
           },
           {
              "category_name": "Misc",
-             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Mkbhd.png"
+             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Misc/Mkbhd.png"
           },
           {
              "category_name": "Photography",
-             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/Photography_9.jpg"
+             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/Photography/Photography_9.jpg"
           },
 	 {
              "category_name": "Community",
-             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Thumb/MateriOS.png"
+             "category_url": "https://raw.githubusercontent.com/hiten1985/Backgrounds/master/Walls/CommunityWalls/MateriOS.png"
           }
 	
 	]
